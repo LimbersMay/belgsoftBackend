99452d59a80a4e8f5fb87131486f3d87c9280826
limberMay <limbermay1@gmail.com>
2023-05-06 13:53:25 -0600

    Variables de entorno definidas en el environment.d.ts

---
+++ environment.d.ts
@@ -0,0 +1,17 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            PORT: string;
+            MYSQL_DATABASE: string;
+            MYSQL_USER: string;
+            MYSQL_PASSWORD: string;
+            MYSQL_HOST: string;
+            MYSQL_PORT: string;
+            NODE_ENV: string;
+        }
+    }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {}
