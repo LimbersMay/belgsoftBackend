8fc5f524404a7311de69ff505fdfc6361d0c9a4d
limberMay <limbermay1@gmail.com>
2023-05-06 14:09:03 -0600

    (models) nombres de las variables de entorno cambiadas con el prefijo DB en lugar de MYSQL

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -1,6 +1,6 @@
-export const PORT = process.env.PORT ?? '';
-export const MYSQL_DATABASE = process.env.MYSQL_DATABASE ?? '';
-export const MYSQL_HOST = process.env.MYSQL_HOST ?? '';
-export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD ?? '';
-export const MYSQL_PORT = process.env.MYSQL_PORT ?? '';
-export const MYSQL_USER = process.env.MYSQL_USER ?? '';
+export const SERVER_PORT = process.env.PORT ?? '';
+export const DB_NAME = process.env.MYSQL_DATABASE ?? '';
+export const DB_HOST = process.env.MYSQL_HOST ?? '';
+export const DB_PASSWORD = process.env.MYSQL_PASSWORD ?? '';
+export const DB_PORT = process.env.MYSQL_PORT ?? '';
+export const DB_USER = process.env.MYSQL_USER ?? '';
