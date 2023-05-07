26b41f10b958ecf6e7af4b00404e812578dfce64
limberMay <limbermay1@gmail.com>
2023-05-06 19:23:22 -0600

    Se exportó la variable de entorno COOKIE_SECRET desde el archivo secrets.ts

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -4,3 +4,5 @@ export const DB_HOST = process.env.MYSQL_HOST ?? '';
 export const DB_PASSWORD = process.env.MYSQL_PASSWORD ?? '';
 export const DB_PORT = process.env.MYSQL_PORT ?? '';
 export const DB_USER = process.env.MYSQL_USER ?? '';
+
+export const COOKIE_SECRET = process.env.COOKIE_SECRET ?? '';
