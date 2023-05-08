5beee130dab515934b4955c8f6ba41a706a00a22
limberMay <limbermay1@gmail.com>
2023-05-08 00:26:19 -0600

    Se hizo return del res.status() del error.handler.ts

--- src/utils/error.handler.ts
+++ src/utils/error.handler.ts
@@ -2,5 +2,5 @@ import {Response} from "express";
 
 export const handleHttp = (res: Response, error: string, errorRaw?: unknown) => {
     console.log(errorRaw);
-    res.status(500).json({error});
+    return res.status(500).json({error});
 }
