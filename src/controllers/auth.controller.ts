2da1465f988c13122739b2d8f10e1e0f60be5e5b
limberMay <limbermay1@gmail.com>
2023-05-08 15:41:09 -0600

    Errores de autenticación obtenidos del AUTH_ERRORS en lugar de USER_ERRORS

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,7 +3,7 @@ import {loginUser, registerUser} from "../services";
 import {Auth} from "../interfaces";
 import {Response} from "express";
 import {handleHttp} from "../utils";
-import {USER_ERRORS} from "../errors/user.errors";
+import {AUTH_ERRORS, USER_ERRORS} from "../errors";
 
 @JsonController('/auth')
 export class AuthController {
@@ -13,7 +13,7 @@ export class AuthController {
         try {
             return await registerUser(body);
         } catch (e) {
-            return handleHttp(res, USER_ERRORS.ERROR_REGISTER_USER, e)
+            return handleHttp(res, AUTH_ERRORS.ERROR_REGISTER_USER, e)
         }
     }
 
@@ -27,13 +27,13 @@ export class AuthController {
                 case USER_ERRORS.USER_NOT_FOUND:
                     return handleHttp(res, USER_ERRORS.USER_NOT_FOUND);
 
-                case USER_ERRORS.INCORRECT_PASSWORD:
-                    return handleHttp(res, USER_ERRORS.INCORRECT_PASSWORD);
+                case AUTH_ERRORS.ERROR_INCORRECT_PASSWORD:
+                    return handleHttp(res, AUTH_ERRORS.ERROR_INCORRECT_PASSWORD);
             }
 
             return responseUser;
         } catch (e) {
-            return handleHttp(res, USER_ERRORS.ERROR_LOGIN_USER, e)
+            return handleHttp(res, AUTH_ERRORS.ERROR_LOGIN_USER, e)
         }
     }
 }
