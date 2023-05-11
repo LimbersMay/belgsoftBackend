5e9872c50a0565e235f1b73faa0478b3d279bbc7
limberMay <limbermay1@gmail.com>
2023-05-11 14:10:42 -0600

    Ruta para obtener todas las órdenes implementado en el controlador

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,12 +1,20 @@
-import {Get, JsonController, UseBefore} from "routing-controllers";
+import {Get, JsonController, Req, Res, UseBefore} from "routing-controllers";
 import {IsAuthenticated} from "../middlewares";
+import {getAllOrders} from "../services/order.service";
+import {handleHttp} from "../utils";
+import {Request, Response} from "express";
 
 @JsonController("/orders")
 export class OrderController {
 
     @Get("/")
     @UseBefore(IsAuthenticated)
-    getAll() {
-        return "This action returns all orders with a valid session";
+    public async getAll(@Res() res: Response) {
+
+        try {
+            return await getAllOrders();
+        } catch (e) {
+            return handleHttp(res, "ERROR_GET_ORDERS", e);
+        }
     }
 }
