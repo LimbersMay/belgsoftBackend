3b00dc91e3e2b14af7069635c1515dd28a5b92d3
limberMay <limbermay1@gmail.com>
2023-05-11 12:36:47 -0600

    Se envolvió la verificación del token en una promesa y se devolvió un objeto que UserResponse como payload

--- src/utils/jwt.handle.ts
+++ src/utils/jwt.handle.ts
@@ -2,13 +2,32 @@ import {sign, verify} from "jsonwebtoken";
 import {JWT_SECRET} from "./secrets";
 import {UserResponse} from "../mappers";
 
-export const generateToken = (user: UserResponse) => {
+interface MyJwtPayload {
+    user: UserResponse;
+    iat: number;
+    exp: number;
+}
+
+export const generateToken = (user: UserResponse): string => {
     return sign({ user }, JWT_SECRET, {
         expiresIn: '2d'
     });
-
 }
 
-export const verifyToken = (token: string) => {
-    return verify(token, JWT_SECRET);
+export const verifyToken = (token: string): Promise<UserResponse> => {
+    return new Promise((resolve, reject) => {
+        verify(token, JWT_SECRET, (err, payload) => {
+            if (err) {
+                return reject(err);
+            }
+
+            if (!payload) {
+                return reject(new Error('No payload'));
+            }
+
+            const myPayload = payload as MyJwtPayload;
+            return resolve(myPayload.user);
+        });
+    });
 }
+
