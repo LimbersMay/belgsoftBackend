f3b2fedb6c2a20ea06dfdf7cb44dfaf2d3fb9cb2
limberMay <limbermay1@gmail.com>
2023-05-11 12:12:30 -0600

    middleware para verificar si un usuario cumple son ciertos roles implementado

--- src/middlewares/role-verify.middleware.ts
+++ src/middlewares/role-verify.middleware.ts
@@ -1,8 +1,34 @@
 import {ExpressMiddlewareInterface} from "routing-controllers";
 import {NextFunction, Request, Response} from "express";
+import {verifyToken} from "../utils";
+
+export const permissionChecker = (requiredRoles: string[]) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        // create the middleware logic
+        const middleware = new PermissionChecker(requiredRoles);
+
+        // use the middleware
+        middleware.use(req, res, next).then();
+    }
+}
 
 export class PermissionChecker implements ExpressMiddlewareInterface {
-    async use(req: Request, res: Response, next: NextFunction) {
+    constructor(private readonly requiredRoles: string[]) {
+        this.requiredRoles = requiredRoles;
+    }
 
+    async use(req: Request, res: Response, next: NextFunction) {
+        const token = req.headers["x-token"];
+
+        const payload = await verifyToken(`${token}`);
+
+        if (!this.requiredRoles.includes(payload.role)) {
+            return next({
+                status: 401,
+                message: "You don't have permission to perform this action"
+            })
+        }
+
+        return next();
     }
 }
