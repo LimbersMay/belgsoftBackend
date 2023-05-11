8b2767a46bb53ad3b7b3edef290da6fd2aa3e055
limberMay <limbermay1@gmail.com>
2023-05-11 14:09:37 -0600

    Función para obtener todas las órdenes implementado

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,4 +1,7 @@
+import {OrderSchema} from "../models";
+import {OrderResponse} from "../mappers/order.response";
 
-export const getOrders = () => {
-
+export const getAllOrders = async () => {
+    const orders = await OrderSchema.findAll({});
+    return orders.map(order => OrderResponse.fromOrder(order));
 }
