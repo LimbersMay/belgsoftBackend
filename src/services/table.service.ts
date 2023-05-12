cfbcf0d6a49b44a4163ef9fdcd3dc18f482e0972
limberMay <limbermay1@gmail.com>
2023-05-12 09:20:51 -0600

    Función para crear una entidad Table en la base de datos agregado (no implementado)

--- src/services/table.service.ts
+++ src/services/table.service.ts
@@ -1,7 +1,13 @@
+import {v4 as uuidv4} from 'uuid';
 import {TableSchema} from "../models";
-import {TableResponse} from "../mappers/table.response";
+import {TableResponse} from "../mappers";
+import {CreateTableDTO} from "../controllers";
 
 export const getAllTables = async() => {
     const tables = await TableSchema.findAll({});
     return tables.map(table => TableResponse.fromTable(table));
+}
+
+export const createTable = async(table: CreateTableDTO) => {
+
 }
