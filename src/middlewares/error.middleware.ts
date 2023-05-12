495d0a453a72068afb0c8794b90b9dbe294d253e
limberMay <limbermay1@gmail.com>
2023-05-11 19:10:12 -0600

    Se agregó un validador para validar la excepción del class-validator en el middleware error.middleware.ts

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,15 +1,36 @@
-import {ExpressErrorMiddlewareInterface, Middleware} from "routing-controllers";
+import {BadRequestError, ExpressErrorMiddlewareInterface, Middleware} from "routing-controllers";
 import {NextFunction, Request, Response} from "express";
 import {HttpErrorHandler} from "../interfaces/httpError.interface";
+import {ValidationError} from "class-validator";
 
 @Middleware({ type: "after" })
 export class ErrorMiddleware implements ExpressErrorMiddlewareInterface {
-        error(error: HttpErrorHandler, request: Request, response: Response, next: NextFunction) {
-            console.log("Error: ", error);
-            response.status(error.status || 500);
-            response.json({
-                error: error.message
-            });
-            next();
+        error(error: HttpErrorHandler | any, request: Request, response: Response, next: NextFunction) {
+
+            if (error.errors) {
+
+                // class-validator error
+                const errors = error.errors.map((err: ValidationError) => {
+                    return {
+                        field: err.property,
+                        constraints: err.constraints
+                    }
+                });
+
+                response.status(400);
+                response.json({
+                    message: error.message,
+                    errors
+                });
+
+            } else {
+                // generic error handler
+                response.status(error.status || 500);
+                response.json({
+                    error: error.message,
+                });
+            }
+
+            return next();
         }
 }
