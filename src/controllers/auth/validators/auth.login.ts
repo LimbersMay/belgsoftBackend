a3e45a74caec7738904dcd803bb5300f7740f256
limberMay <limbermay1@gmail.com>
2023-05-15 00:29:53 -0600

    Clase AuthLoginDTO creado para validar el correo y contraseña que envia el cliente

---
+++ src/controllers/auth/validators/auth.login.ts
@@ -0,0 +1,19 @@
+import {IsEmail, IsNotEmpty, IsString} from "class-validator";
+
+export class AuthLoginDTO {
+    @IsEmail({}, {
+        message: 'Invalid email'
+    })
+    @IsNotEmpty({
+        message: 'Email is required'
+    })
+    email!: string;
+
+    @IsString({
+        message: 'Password must be a string'
+    })
+    @IsNotEmpty({
+        message: 'Password is required'
+    })
+    password!: string;
+}
