baca3c832c64fbc9d7459b6cd967b3576fb1e7f4
limberMay <limbermay1@gmail.com>
2023-05-15 17:19:24 -0600

    controlador para actualizar una mesa (Table) implementado

--- src/controllers/table/table.controller.ts
+++ src/controllers/table/table.controller.ts
@@ -1,10 +1,23 @@
-import {Authorized, Body, Get, JsonController, Post, Res, UseBefore} from "routing-controllers";
+import {
+    Authorized,
+    Body,
+    CurrentUser,
+    Get,
+    JsonController,
+    Param,
+    Post,
+    Put,
+    Res,
+    UseBefore
+} from "routing-controllers";
 import {IsAuthenticated} from "../../middlewares";
 import {Response} from "express";
-import {getAllTables} from "../../services";
+import {createTable, findAllTables, updateTable} from "../../services";
 import {handleHttp} from "../../utils";
 import {CreateTableDTO} from "./validators/table.create";
 import {TABLE_ERRORS} from "../../errors/table.errors";
+import {UpdateTableDTO} from "./validators/table.update";
+import {UserResponse} from "../../mappers";
 
 @JsonController('/tables')
 export class TableController {
@@ -13,20 +26,44 @@ export class TableController {
     @UseBefore(IsAuthenticated)
     public async getAll(@Res() res: Response) {
         try {
-            return await getAllTables();
+            return await findAllTables();
         } catch (e) {
-            return handleHttp(res, TABLE_ERRORS.ERROR_CANNOT_GET_TABLES, e);
+            return handleHttp(res, TABLE_ERRORS.TABLE_ERROR_CANNOT_GET_TABLES, e);
         }
     }
 
     @UseBefore(IsAuthenticated)
     @Authorized('ADMIN')
     @Post('/')
-    public async createTable(@Res() res: Response, @Body({validate: true}) createTableDTO: CreateTableDTO) {
+    public async createTable(@Res() res: Response, @Body({validate: true}) createTableDTO: CreateTableDTO, @CurrentUser({required: true}) user: UserResponse) {
         try {
-            return await getAllTables();
+            const responseTable = await createTable(createTableDTO, user.branchId);
+
+            if (typeof responseTable === 'string') {
+                return handleHttp(res, responseTable);
+            }
+
+            return responseTable;
         } catch (e) {
             return handleHttp(res, TABLE_ERRORS.ERROR_CANNOT_CREATE_TABLE, e);
+        }
+    }
+
+    @UseBefore(IsAuthenticated)
+    @Authorized('ADMIN')
+    @Put('/:id')
+    public async updateTable(@Res() res: Response, @Param('id') id: string, @Body({ validate: true}) updateTableDTO: UpdateTableDTO) {
+
+        try {
+            const responseTable = await updateTable(id, updateTableDTO);
+
+            if (typeof responseTable === 'string') {
+                return handleHttp(res, responseTable);
+            }
+
+            return responseTable;
+        } catch (e) {
+            return handleHttp(res, TABLE_ERRORS.ERROR_CANNOT_UPDATE_TABLE, e);
         }
     }
 }
