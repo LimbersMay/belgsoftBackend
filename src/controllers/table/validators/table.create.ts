5a74031d203bb8ced0bfefe10b15577ebb1c3d6e
limberMay <limbermay1@gmail.com>
2023-05-15 11:55:02 -0600

    Validaciones para verificar que los campos message y customers no estén vacios en el table.create.ts

--- src/controllers/table/validators/table.create.ts
+++ src/controllers/table/validators/table.create.ts
@@ -1,13 +1,19 @@
-import {IsNumber, IsString} from "class-validator";
+import {IsNotEmpty, IsNumber, IsString} from "class-validator";
 
 export class CreateTableDTO {
     @IsString({
         message: "Table number must be a string"
     })
+    @IsNotEmpty({
+        message: "Table number is required"
+    })
     number!: string;
 
     @IsNumber({}, {
         message: "Table customers must be a number"
+    })
+    @IsNotEmpty({
+        message: "Table customers is required"
     })
     customers!: number;
 }
