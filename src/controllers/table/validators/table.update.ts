9c6ee637bce9239e20d0e641cc34f3865f0d5e02
limberMay <limbermay1@gmail.com>
2023-05-16 09:38:48 -0600

    (controllers/table/validators) Se marcó los atributos de actualización de una mesa como opcionales

--- src/controllers/table/validators/table.update.ts
+++ src/controllers/table/validators/table.update.ts
@@ -1,19 +1,32 @@
-import {IsNotEmpty, IsNumber, IsString} from "class-validator";
+import {IsNotEmpty, IsNumber, IsOptional, IsString, ValidateIf} from "class-validator";
+import {IsTableExist} from "./table-existance";
+import {TABLE_ERRORS} from "../../../errors/table.errors";
+
+export class UpdateTableIdDTO {
+    @IsString({
+        message: 'Id must be a string'
+    })
+    @IsNotEmpty({
+        message: 'Id is required'
+    })
+    @IsTableExist({
+        message: TABLE_ERRORS.TABLE_ERROR_TABLE_NOT_FOUND
+    })
+    id!: string;
+}
 
 export class UpdateTableDTO {
+
+    @IsOptional()
+    @ValidateIf((_v, value) => value !== undefined)
     @IsString({
         message: 'Number must be a string'
     })
-    @IsNotEmpty({
-        message: 'Number is required'
-    })
     number!: string;
 
+    @IsOptional()
     @IsNumber({}, {
         message: 'Customers must be a number'
-    })
-    @IsNotEmpty({
-        message: 'Customers is required'
     })
     customers!: number;
 }
