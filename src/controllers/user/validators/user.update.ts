c7a27fdf0f1e9ae1fd1628380f87f28c351f4b3a
limberMay <limbermay1@gmail.com>
2023-05-15 22:38:37 -0600

    (controllers/user) Clases validadores UpdateUserDTO y updateUserIdDTO creados para validar el Body y el param al actualizar un usuario

---
+++ src/controllers/user/validators/user.update.ts
@@ -0,0 +1,46 @@
+import {IsEmail, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength, ValidateIf} from "class-validator";
+import {IsUserExist} from "./user-exists";
+import {IsUserAlreadyExist} from "../../validators/email-exists";
+
+export class UpdateUserIdDTO {
+        @IsString({
+            message: 'Id must be a string'
+        })
+        @IsNotEmpty({
+            message: 'Id is required'
+        })
+        @IsUserExist({
+            message: 'User not found'
+        })
+        id!: string;
+}
+
+export class UpdateUserDTO {
+
+    @IsOptional()
+    @IsString({
+        message: 'Name must be a string'
+    })
+    @ValidateIf(o => o.name !== undefined)
+    name?: string;
+
+    @IsOptional()
+    @ValidateIf(o => o.email !== undefined)
+    @IsEmail({},{
+        message: 'Name must be a string'
+    })
+    @IsUserAlreadyExist({
+        message: 'Email already registered'
+    })
+    email?: string;
+
+    @IsOptional()
+    @ValidateIf(o => o.password !== undefined)
+    @IsString({
+        message: 'Name must be a string'
+    })
+    @IsNotEmpty({
+        message: 'Name is required'
+    })
+    password?: string;
+}
