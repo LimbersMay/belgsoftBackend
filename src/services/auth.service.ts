4d9ad2d956537124bdf46929e4e8890cf098101b
limberMay <limbermay1@gmail.com>
2023-05-15 21:52:33 -0600

    (services/auth) Se eliminó la comprobación de si existe o no un usuario con un email registrado antes de hacer el registro

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -6,15 +6,6 @@ import {compareHash, encrypt, generateToken} from "../utils";
 import {AUTH_ERRORS, USER_ERRORS} from "../errors";
 import {RoleSchema, UserTypeSchema, UserStateSchema} from "../models";
 export const registerUser = async (authProps: Auth) => {
-
-    // check if user exists
-    const userExists = await UserSchema.findOne({
-        where: { email: authProps.email }
-    });
-
-
-    if (userExists) return AUTH_ERRORS.AUTH_EMAIL_ALREADY_EXISTS;
-
     const encryptedPassword = await encrypt(authProps.password);
     const user = await UserSchema.create({
         ...authProps,
@@ -33,7 +24,7 @@ export const registerUser = async (authProps: Auth) => {
         ]
     });
 
-    if (!newUser) return AUTH_ERRORS.AUTH_CANNOT_REGISTER_USER;
+    if (!newUser) return AUTH_ERRORS.AUTH_ERROR_CANNOT_REGISTER_USER;
 
     return UserResponse.fromUser(newUser);
 }
@@ -52,7 +43,7 @@ export const loginUser = async ({ email, password }: { email: string, password: 
     if (!user) return USER_ERRORS.USER_NOT_FOUND;
 
     const isPasswordValid = await compareHash(password, user.password);
-    if (!isPasswordValid) return AUTH_ERRORS.AUTH_INCORRECT_PASSWORD;
+    if (!isPasswordValid) return AUTH_ERRORS.AUTH_ERROR_INCORRECT_PASSWORD;
 
     const userResponse = UserResponse.fromUser(user);
 
