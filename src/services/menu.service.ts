9e45ae0a69ced920d1b75dcf9d47adf15a203b26
limberMay <limbermay1@gmail.com>
2023-05-16 23:54:01 -0600

    (services menu) función createMenu implementado y se configuró findAllMenu para devolver objetos asociados

--- src/services/menu.service.ts
+++ src/services/menu.service.ts
@@ -1,7 +1,34 @@
-import {MenuSchema} from "../models";
-import {MenuResponse} from "../mappers/menu.response";
+import { v4 as uuidv4 } from "uuid";
+import {CategorySchema, MenuSchema} from "../models";
+import {MenuResponse} from "../mappers";
+import {CreateMenuDTO} from "../controllers/menu/validations/menu.create";
 
 export const findAllMenu = async (): Promise<MenuResponse[]> => {
-    const menu = await MenuSchema.findAll({});
+    const menu = await MenuSchema.findAll({
+        include: [
+            {model: CategorySchema, as: 'category'}
+        ]
+    });
     return menu.map(item => MenuResponse.fromMenu(item));
+}
+
+export const createMenu = async (menuDTO: CreateMenuDTO, branchId: string) => {
+    const newMenu = await MenuSchema.create({
+        menuId: uuidv4(),
+        branchId,
+        categoryId: menuDTO.categoryId,
+        name: menuDTO.name,
+        description: menuDTO.description,
+        price: menuDTO.price,
+        image: menuDTO.image,
+        isAvailable: menuDTO.isAvailable,
+    });
+
+    await newMenu.reload({
+        include: [
+            {model: CategorySchema, as: 'category'}
+        ]
+    })
+
+    return MenuResponse.fromMenu(newMenu);
 }
