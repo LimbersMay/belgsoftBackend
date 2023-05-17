0715474deb6c7457ee328502813eeda6dd3014cd
limberMay <limbermay1@gmail.com>
2023-05-16 23:52:08 -0600

    (interfaces menu) campos createdAt y updatedAt marcados como opcionales y se agregó el atributo branchId

--- src/interfaces/menu.interface.ts
+++ src/interfaces/menu.interface.ts
@@ -1,11 +1,12 @@
 export interface Menu {
     menuId: string;
+    branchId: string;
     categoryId: string;
     name: string;
     description: string;
     price: number;
     image: string;
     isAvailable: boolean;
-    createdAt: Date;
-    updatedAt: Date;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
