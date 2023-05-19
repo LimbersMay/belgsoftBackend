d55fe86944ed2d46b9ef4b5908e171434dcc4890
limberMay <limbermay1@gmail.com>
2023-05-18 20:59:50 -0600

    (services user) función findUSerByAdminId agregado y se validó el atributo createdByUserId al actualizar un usuario

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,5 @@
 import {UserResponse} from "../mappers";
-import {USER_ERRORS} from "../errors";
+import {UserError} from "../errors";
 import {UserSchema, RoleSchema, UserTypeSchema, UserStateSchema} from "../models";
 import {UpdateUserDTO} from "../controllers/user/validators/user.update";
 import {encrypt} from "../utils";
@@ -16,7 +16,7 @@ export const getUserById = async (userId: string) => {
         });
 
     if (!user) {
-        return USER_ERRORS.USER_NOT_FOUND;
+        return UserError.USER_NOT_FOUND;
     }
 
     return UserResponse.fromUser(user);
@@ -34,8 +34,10 @@ export const findUserById = async (userId: string) => {
     });
 }
 
-export const getAllUsers = async () => {
+export const getAllUsersByAdminId = async (createdByUserId: string) => {
+
     const users = await UserSchema.findAll({
+        where: {createdByUserId},
         include: [
             {model: RoleSchema, as: 'role'},
             {model: UserTypeSchema, as: 'userType'},
@@ -46,14 +48,30 @@ export const getAllUsers = async () => {
     return users.map(user => UserResponse.fromUser(user));
 }
 
-export const updateUser = async (userId: string, updateUserDTO: UpdateUserDTO) => {
+export const findUserByAdminId = async (userId: string, createdByUserId: string) => {
+    return await UserSchema.findOne({where: {userId, createdByUserId}});
+}
+
+export const updateUser = async (userId: string, adminId: string | undefined, updateUserDTO: UpdateUserDTO) => {
 
     // If the password is updated, encrypt it
     if (updateUserDTO.password) {
         updateUserDTO.password = await encrypt(updateUserDTO.password);
     }
 
+    const whereQuery: Record<string, string> = {
+        userId
+    };
+
+    if (adminId) {
+        whereQuery['createdByUserId'] = adminId;
+    }
+
     return await UserSchema.update({
         ...updateUserDTO
-    }, {where: {userId}});
+    }, {
+        where: {
+            ...whereQuery
+        }
+    });
 }
