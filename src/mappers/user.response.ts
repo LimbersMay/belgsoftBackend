33d3b22a763c19d1071394d7ef100cb46d5dc31a
limberMay <limbermay1@gmail.com>
2023-05-18 20:58:32 -0600

    (mappers) atributo createdByUserId añadido a la clase UserResponse

--- src/mappers/user.response.ts
+++ src/mappers/user.response.ts
@@ -2,6 +2,7 @@ import {UserSchema} from "../models";
 
 export class UserResponse {
     userId: string;
+    createdByUserId: string | undefined;
     branchId: string;
     name: string;
     email: string;
@@ -9,8 +10,9 @@ export class UserResponse {
     userType: string;
     userState: string;
 
-    constructor(userId: string, branchId: string, name: string, email: string, role: string, userType: string, userState: string) {
+    constructor(userId: string,createdByUserId: string | undefined, branchId: string, name: string, email: string, role: string, userType: string, userState: string) {
         this.userId = userId;
+        this.createdByUserId = createdByUserId;
         this.branchId = branchId;
         this.name = name;
         this.email = email;
@@ -22,6 +24,7 @@ export class UserResponse {
     public static fromUser(user: UserSchema): UserResponse {
         return new UserResponse(
             user.userId,
+            user.createdByUserId,
             user.branchId,
             user.name,
             user.email,
