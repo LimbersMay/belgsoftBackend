3652fe08f75249ccb3e798693cc3407e438ec1aa
limberMay <limbermay1@gmail.com>
2023-05-20 16:41:39 -0600

    (area) columna branchId agregado al AreaSchema

--- src/models/area.schema.ts
+++ src/models/area.schema.ts
@@ -1,10 +1,14 @@
 import {Column, Model, PrimaryKey, Table} from "sequelize-typescript";
+import {Area} from "../interfaces/area.interface";
 
 @Table
-export class AreaSchema extends Model{
+export class AreaSchema extends Model<Area>{
     @PrimaryKey
     @Column
     areaId!: string;
+
+    @Column
+    branchId!: string;
 
     @Column
     name!: string;
