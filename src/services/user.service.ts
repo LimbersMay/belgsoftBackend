33314b87a88295497470d5eb5ff3daceb8e91dda
limberMay <limbermay1@gmail.com>
2023-05-20 16:33:23 -0600

    (controllers user) se borraron las validaciones manuales y se abstrajo toda la lógica a una clase userSpecificationBuilder

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,21 +1,17 @@
-import {Op, WhereOptions} from "sequelize";
 import {UserResponse} from "../mappers";
 import {UserSchema, RoleSchema, UserTypeSchema, UserStateSchema} from "../models";
 import {UpdateUserDTO} from "../controllers/user/validators/user.update";
 import {encrypt} from "../utils";
 import {User} from "../interfaces";
 import {
-    AndSpecification,
-    NotSpecification,
-    OrSpecification,
-    Specification,
-
-    CreatedByAdminIdSpecification,
-    UserEmailSpecification,
-    UserIdSpecification
+    Specification
 } from "../specifications";
+import {UserSpecificationBuilder} from "../specifications/sequelize/user-specifications.builder";
 
 type UserSpecification = Specification<User> | Specification<User>[];
+
+// User specification validator
+const userSpecificationBuilder = new UserSpecificationBuilder();
 
 export const updateUser = async (updateUserDTO: UpdateUserDTO, specifications: UserSpecification) => {
 
@@ -24,7 +20,7 @@ export const updateUser = async (updateUserDTO: UpdateUserDTO, specifications: U
         updateUserDTO.password = await encrypt(updateUserDTO.password);
     }
 
-    const whereQuery = buildWhereClauseFromSpecifications(specifications);
+    const whereQuery = userSpecificationBuilder.buildWhereClauseFromSpecifications(specifications);
 
     return await UserSchema.update({
         ...updateUserDTO
@@ -36,7 +32,7 @@ export const updateUser = async (updateUserDTO: UpdateUserDTO, specifications: U
 }
 
 export const findUser = async (specification: UserSpecification) => {
-    const whereQuery = buildWhereClauseFromSpecifications(specification);
+    const whereQuery = userSpecificationBuilder.buildWhereClauseFromSpecifications(specification);
     return await UserSchema.findOne({
         where: whereQuery,
         include: [
@@ -46,7 +42,7 @@ export const findUser = async (specification: UserSpecification) => {
 }
 
 export const findAllUsers = async (specification: UserSpecification) => {
-    const where = buildWhereClauseFromSpecifications(specification);
+    const where = userSpecificationBuilder.buildWhereClauseFromSpecifications(specification);
     const users = await UserSchema.findAll({
         where,
         include: [
@@ -57,52 +53,4 @@ export const findAllUsers = async (specification: UserSpecification) => {
     });
 
     return users.map(user => UserResponse.fromUser(user));
-}
-
-const buildWhereClauseFromSpecifications = (specifications: UserSpecification) => {
-    const where: WhereOptions = {};
-
-    // if we get a single specification, convert it to an array
-    const specs = Array.isArray(specifications) ? specifications : [specifications];
-
-    for (const specification of specs) {
-        const clause = buildWhereClauseFromSpecification(specification);
-        Object.assign(where, clause);
-    }
-
-    return where;
-}
-
-const buildComplexSpecification = <T>(specification: Specification<T>): WhereOptions<T> => {
-    // If you have complex specifications, you can use the following
-    if (specification instanceof AndSpecification) {
-        const oneClause = buildWhereClauseFromSpecification(specification.one);
-        const otherClause = buildWhereClauseFromSpecification(specification.other);
-        return { [Op.and]: [oneClause, otherClause] };
-    } else if (specification instanceof OrSpecification) {
-        const oneClause = buildWhereClauseFromSpecification(specification.one);
-        const otherClause = buildWhereClauseFromSpecification(specification.other);
-        return { [Op.or]: [oneClause, otherClause] };
-    } else if (specification instanceof NotSpecification) {
-        const wrappedClause = buildWhereClauseFromSpecification(specification.wrapped);
-        return { [Op.not]: wrappedClause };
-    }
-
-    // if specification is not recognized, return empty where clause
-    return {};
-}
-
-const buildWhereClauseFromSpecification = (specification: Specification<User>): WhereOptions<User> => {
-    // build the where clause for the specification
-
-    if (specification instanceof UserIdSpecification) {
-        return {userId: specification.userId};
-    } else if (specification instanceof UserEmailSpecification) {
-        return {email: specification.email};
-    } else if (specification instanceof CreatedByAdminIdSpecification) {
-        return {createdByUserId: specification.adminId};
-    }
-
-    // If you have complex specifications, you can use the following
-    return buildComplexSpecification<User>(specification);
 }
