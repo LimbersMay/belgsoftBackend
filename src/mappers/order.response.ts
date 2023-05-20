aeee069d56a435a15a2d0c39271d055b87f9560f
limberMay <limbermay1@gmail.com>
2023-05-20 15:33:49 -0600

    (mappers order) atributos tableId, userId, orderStatusId y areaId reemplazados por table, user, orderStatus y area en texto plano en lugar de devolver el id

--- src/mappers/order.response.ts
+++ src/mappers/order.response.ts
@@ -3,53 +3,49 @@ import {OrderSchema} from "../models";
 interface OrderResponseProps {
     orderId: string;
     customerName?: string;
-    tableId: string;
-    userId: string;
-    orderStatusId: string;
-    areaId: string;
+    table: string;
+    userName: string;
+    orderStatus: string;
+    area: string;
     price: number;
     quantity: number;
     createdAt: Date;
-    updatedAt: Date;
 }
 
 export class OrderResponse {
     private orderId: string;
     private customerName?: string;
-    private tableId: string;
-    private userId: string;
-    private orderStatusId: string;
-    private areaId: string;
+    private table: string;
+    private userName: string;
+    private orderStatus: string;
+    private area: string;
     private price: number;
     private quantity: number;
     private createdAt: Date;
-    private updatedAt: Date;
 
-    constructor({ orderId, customerName, tableId, userId, orderStatusId, areaId, price, quantity, createdAt, updatedAt }: OrderResponseProps) {
+    constructor({ orderId, customerName, table, userName, orderStatus, area, price, quantity, createdAt }: OrderResponseProps) {
         this.orderId = orderId;
         this.customerName = customerName;
-        this.tableId = tableId;
-        this.userId = userId;
-        this.orderStatusId = orderStatusId;
-        this.areaId = areaId;
+        this.table = table;
+        this.userName = userName;
+        this.orderStatus = orderStatus;
+        this.area = area;
         this.price = price;
         this.quantity = quantity;
         this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
     }
 
     public static fromOrder(orderModel: OrderSchema) {
         return new OrderResponse({
             orderId: orderModel.orderId,
             customerName: orderModel.customerName,
-            tableId: orderModel.tableId,
-            userId: orderModel.userId,
-            orderStatusId: orderModel.orderStatusId,
-            areaId: orderModel.areaId,
+            table: orderModel.tableId,
+            userName: orderModel.userId,
+            orderStatus: orderModel.orderStatusId,
+            area: orderModel.areaId,
             price: orderModel.price,
             quantity: orderModel.quantity,
             createdAt: orderModel.createdAt,
-            updatedAt: orderModel.updatedAt
         });
     }
 }
