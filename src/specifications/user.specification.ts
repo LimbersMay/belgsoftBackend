ca7bb5dc0dddc64932e662df3535c29a240a0ec4
limberMay <limbermay1@gmail.com>
2023-05-20 15:29:43 -0600

    (controllers/user) specificaciones para queries de usuario creados (id, email, createdByAdminId)

---
+++ src/specifications/user.specification.ts
@@ -0,0 +1,41 @@
+import {AbstractSpecification} from "./generic-specification";
+import {User} from "../interfaces";
+
+export class UserIdSpecification extends AbstractSpecification<User> {
+    public readonly userId: string;
+
+    public constructor(userId: string) {
+        super();
+        this.userId = userId;
+    }
+
+    public isSatisfiedBy(candidate: User): boolean {
+        return candidate.userId === this.userId;
+    }
+}
+
+export class UserEmailSpecification extends AbstractSpecification<User> {
+    public readonly email: string;
+
+    public constructor(email: string) {
+        super();
+        this.email = email;
+    }
+
+    public isSatisfiedBy(candidate: User): boolean {
+        return candidate.email === this.email;
+    }
+}
+
+export class CreatedByAdminIdSpecification extends AbstractSpecification<User> {
+    public readonly adminId: string;
+
+    public constructor(adminId: string) {
+        super();
+        this.adminId = adminId;
+    }
+
+    public isSatisfiedBy(candidate: User): boolean {
+        return candidate.createdByUserId === this.adminId;
+    }
+}
