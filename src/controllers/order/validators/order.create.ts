a45238d8737a19fd1e048b7a97802003cbd35116
limberMay <limbermay1@gmail.com>
2023-05-20 20:52:54 -0600

    (controllers order) clase OrderCreateDTO Creado para validar todos los atributos al crear ordenes

---
+++ src/controllers/order/validators/order.create.ts
@@ -0,0 +1,59 @@
+import {IsNotEmpty, IsNumber, IsOptional, IsString, IsUUID, ValidateIf} from "class-validator";
+
+export class OrderCreateDTO {
+    @IsUUID(4, {
+        message: 'orderId is not a valid uuid'
+    })
+    @IsNotEmpty({
+        message: 'orderId is required'
+    })
+    orderId!: string;
+
+    @IsUUID(4, {
+        message: 'menuId is not a valid uuid'
+    })
+    menuId!: string;
+
+    @IsOptional()
+    @ValidateIf(o => o.customerName !== undefined)
+    @IsNotEmpty({
+        message: 'customerName is required'
+    })
+    @IsString({
+        message: 'customerName must be a string'
+    })
+    customerName?: string;
+
+    @IsUUID(4, {
+        message: 'areaId is not a valid uuid'
+    })
+    areaId!: string;
+
+    @IsUUID(4, {
+        message: 'tableId is not a valid uuid'
+    })
+    tableId!: string;
+
+    @IsUUID(4, {
+        message: 'userId is not a valid uuid'
+    })
+    userId!: string;
+
+    @IsUUID(4, {
+        message: 'orderStatusId is not a valid uuid'
+    })
+    orderStatusId!: string;
+
+    @IsNotEmpty({
+        message: 'price is required'
+    })
+    @IsNumber({}, {
+        message: 'price must be a number'
+    })
+    price!: number;
+
+    @IsNotEmpty({
+        message: 'quantity is required'
+    })
+    quantity!: number;
+}
