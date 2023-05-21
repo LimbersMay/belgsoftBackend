7439da21e436246833e9b3b1ecea5d102c373bbe
limberMay <limbermay1@gmail.com>
2023-05-20 18:04:41 -0600

    AreaController importado desde el index de controllers

---
+++ src/controllers/area/area.controller.ts
@@ -0,0 +1,39 @@
+import {Authorized, Body, CurrentUser, Get, JsonController, Post, Res, UseBefore} from "routing-controllers";
+import {IsAuthenticated} from "../../middlewares";
+import {handleHttp} from "../../utils";
+import {Response} from "express";
+import {AreaErrors} from "../../errors/area.errors";
+import {createArea, findAllArea} from "../../services/area.service";
+import {UserResponse} from "../../mappers";
+import {BranchIdSpecification} from "../../specifications";
+import {CreateAreaDTO} from "./validations/area.create";
+
+@JsonController('/area')
+@UseBefore(IsAuthenticated)
+export class AreaController {
+    constructor() {}
+
+    @Get('/')
+    @Authorized(['ADMIN', 'USER', 'SUPER_USER'])
+    public async getAll(@Res() res: Response, @CurrentUser() { branchId }: UserResponse) {
+        try {
+            return await findAllArea(new BranchIdSpecification(branchId));
+        } catch (e) {
+            return handleHttp(res, AreaErrors.AREA_ERROR_CANNOT_GET_AREAS, e);
+        }
+    }
+
+    @Post('/')
+    @Authorized(['ADMIN', 'SUPER_USER'])
+    public async create(
+        @Res() res: Response,
+        @Body({validate: true}) createAreaDTO: CreateAreaDTO,
+        @CurrentUser() { branchId }: UserResponse
+    ) {
+        try {
+            return await createArea(createAreaDTO, branchId);
+        } catch (e) {
+            return handleHttp(res, AreaErrors.AREA_ERROR_CANNOT_CREATE_AREA, e);
+        }
+    }
+}
