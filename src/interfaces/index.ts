65ec5f87f50aa7907f2ff4f9cd5b07d0a3a28f44
limberMay <limbermay1@gmail.com>
2023-05-20 20:25:11 -0600

    (interfaces) interfaces de area y order exportadas en el archivo de barril

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,5 @@ export * from "./auth.interface";
 export * from "./user.interface";
 export * from "./table.interface";
 export * from "./httpError.interface";
+export * from "./area.interface";
+export * from "./order.interface";
