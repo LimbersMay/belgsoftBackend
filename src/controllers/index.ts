2ec06a7a46f5f1386c93d79cb8721000e97ea25d
limberMay <limbermay1@gmail.com>
2023-05-20 20:57:49 -0600

    (controllers) archivos de barril actualizados

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,6 @@
 export * from "./user/user.controller";
 export * from "./auth";
-export * from "./order.controller";
+export * from "./order/order.controller";
 export * from "./table";
 export * from "./menu";
 export * from './area'
