59e79be5bb49bda85e18aeb7697b00b17f5d1773
limberMay <limbermay1@gmail.com>
2023-05-21 17:08:44 -0600

    (specifications) archivo de barril actualizado

--- src/specifications/sequelize/index.ts
+++ src/specifications/sequelize/index.ts
@@ -1,3 +1,4 @@
 export * from "./specifications.builder";
 export * from "./area-specifications.builder";
 export * from "./user-specifications.builder";
+export * from "./order-specification.builder";
