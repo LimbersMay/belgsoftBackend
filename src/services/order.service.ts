533024ccac581cd7333d314d243375cc74234d61
limberMay <limbermay1@gmail.com>
2023-05-20 20:50:38 -0600

    (services order) función findAllOrders implementado

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,7 +1,18 @@
 import {OrderSchema} from "../models";
 import {OrderResponse} from "../mappers";
+import {Specification} from "../specifications";
+import {OrderSpecificationBuilder} from "../specifications/sequelize/order-specification.builder";
 
-export const getAllOrders = async () => {
-    const orders = await OrderSchema.findAll({});
+type OrderSpecification = Specification<string> | Specification<string>[];
+
+const orderSpecificationBuilder = new OrderSpecificationBuilder();
+
+export const findAllOrders = async (specifications: OrderSpecification) => {
+    const whereQuery = orderSpecificationBuilder.buildWhereClauseFromSpecifications(specifications);
+
+    const orders = await OrderSchema.findAll({
+        where: whereQuery
+    });
+
     return orders.map(order => OrderResponse.fromOrder(order));
 }
