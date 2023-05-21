91660a6363f568bcb930b6f8b6b04b9456aeb27a
limberMay <limbermay1@gmail.com>
2023-05-20 23:26:47 -0600

    (services order) función createOrder implementado

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,7 +1,9 @@
+import { v4 as uuidv4 } from 'uuid';
 import {OrderSchema} from "../models";
 import {OrderResponse} from "../mappers";
 import {Specification} from "../specifications";
 import {OrderSpecificationBuilder} from "../specifications/sequelize/order-specification.builder";
+import {CreateOrderDTO} from "../controllers/order/validators/order.create";
 
 type OrderSpecification = Specification<string> | Specification<string>[];
 
@@ -15,4 +17,15 @@ export const findAllOrders = async (specifications: OrderSpecification) => {
     });
 
     return orders.map(order => OrderResponse.fromOrder(order));
+}
+
+export const createOrder = async (order: CreateOrderDTO, branchId: string) => {
+
+    const orderInstance = await OrderSchema.create({
+        orderId: uuidv4(),
+        branchId,
+        ...order
+    });
+
+    return OrderResponse.fromOrder(orderInstance);
 }
