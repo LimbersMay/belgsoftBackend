35f20f9e98069129d4419784cdb1152e948d7dc7
limberMay <limbermay1@gmail.com>
2023-05-29 17:48:19 -0600

    (specifications ) se exportaron todos los modulos de category y menu specification

--- src/specifications/index.ts
+++ src/specifications/index.ts
@@ -2,3 +2,5 @@ export * from './user.specification';
 export * from './generic-specification';
 export * from './branch.specification';
 export * from './order.specification';
+export * from './category.specification';
+export * from './menu.specification';
