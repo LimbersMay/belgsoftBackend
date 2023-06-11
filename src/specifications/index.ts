7e9337cb9c19fde8c5dd7119e61c069b98cdfae0
limberMay <limbermay1@gmail.com>
2023-06-11 07:30:37 -0600

    (specifications index) se exportaron todos las exportaciones de table.specification, area.specification y sequelize

--- src/specifications/index.ts
+++ src/specifications/index.ts
@@ -4,3 +4,6 @@ export * from './branch.specification';
 export * from './order.specification';
 export * from './category.specification';
 export * from './menu.specification';
+export * from './table.specification';
+export * from './area.specification';
+export * from './sequelize';
