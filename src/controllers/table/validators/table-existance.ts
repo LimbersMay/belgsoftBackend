521aae94d005567d49765a0cd13e1cbb2281f596
LimbersMay <limbermay1@gmail.com>
2023-10-14 22:34:04 -0600

    (validators table-existance) Se refactorizaron los validadores y se hizo uso del tipo Criteria y los specification para construir dinámicamente la query del validador

--- src/controllers/table/validators/table-existance.ts
+++ src/controllers/table/validators/table-existance.ts
@@ -5,52 +5,61 @@ import {
     ValidatorConstraintInterface,
     ValidationArguments,
 } from 'class-validator';
-import {findTableByQuery} from "../../../services";
+import {findOneTable} from "../../../services";
+import {Criteria} from "../../../specifications";
 
-@ValidatorConstraint({ async: true })
-export class IsTableExistConstraint implements ValidatorConstraintInterface {
+@ValidatorConstraint({async: true})
+export class DoesTableExistConstraint implements ValidatorConstraintInterface {
     async validate(value: string, args: ValidationArguments) {
 
-        const fieldName = args.property;
-        let query: Record<string, string> = {}
+        /*
+            args.constraints[0] is a function that returns another function, which is a specification
 
-        if (fieldName === 'id') {
-            query = {
-                tableId: value
-            }
-        } else {
-            query[fieldName] = value;
-        }
+              Example:
+                  @DoesTableWithQueryExist((value: string) => new TableNumberSpecification(value))
+                  number!: string;
 
-        const table = await findTableByQuery(query);
+                  args.constraints[0] is (value: string) => new TableNumberSpecification(value)
+                  args.constraints[0](value) is new TableNumberSpecification(value)
 
+             However, the value is what is passed to the validator, so it is the table number
+         */
+
+        const specification = args.constraints[0](value);
+        const table = await findOneTable(specification);
         return !!table;
     }
 }
 
-export function IsTableExist(validationOptions?: ValidationOptions) {
+export function DoesTableWithQueryExist(
+    specificationFactory: (value: string) => Criteria,
+    validationOptions?: ValidationOptions
+) {
 
     return function (object: Object, propertyName: string) {
         registerDecorator({
             target: object.constructor,
             propertyName: propertyName,
             options: validationOptions,
-            constraints: [],
-            validator: IsTableExistConstraint,
+            constraints: [specificationFactory],
+            validator: DoesTableExistConstraint,
         });
     };
 }
 
-export function IsTableDoesNotExist(validationOptions?: ValidationOptions) {
+export function DoesTableWithQueryDoesNotExist(
+    specificationFactory: (value: string) => Criteria,
+    validationOptions?: ValidationOptions
+) {
     return function (object: Object, propertyName: string) {
         registerDecorator({
             target: object.constructor,
             propertyName: propertyName,
             options: validationOptions,
-            constraints: [],
+            constraints: [specificationFactory],
             validator: {
                 async validate(value: string, args: ValidationArguments) {
-                    const isExist = await new IsTableExistConstraint().validate(value, args);
+                    const isExist = await new DoesTableExistConstraint().validate(value, args);
                     return !isExist;
                 }
             }
