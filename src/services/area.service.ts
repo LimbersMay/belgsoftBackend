70946ddb8d349753d9abeafcda10014d69cb1c9e
LimbersMay <limbermay1@gmail.com>
2023-10-19 08:32:07 -0600

    (area.service) Se manejaron los posibles errores con el objeto Result

--- src/services/area.service.ts
+++ src/services/area.service.ts
@@ -1,4 +1,5 @@
 import {v4 as uuidv4} from "uuid";
+import {Err, Ok, Result} from "ts-results-es";
 import {AreaSchema} from "../models";
 import {CreateAreaDTO} from "../controllers/area/validations/area.create";
 import {
@@ -7,42 +8,87 @@ import {
 } from "../specifications";
 import {AreaResponse} from "../mappers";
 import {UpdateAreaDTO} from "../controllers/area/validations/area.update";
+import {AreaErrors} from "../errors/area.errors";
+import {promiseHandler} from "../helpers/promiseHandler";
 
 const specificationBuilder = new SequelizeSpecificationBuilder();
 
-export const findAllAreas = async (specifications: Criteria): Promise<AreaResponse[]> => {
+export const findAllAreas = async (specifications: Criteria): Promise<Result<AreaResponse[], Error>> => {
     const whereQuery = specificationBuilder.buildWhereClauseFromSpecifications(specifications);
 
-    const areas = await AreaSchema.findAll({where: whereQuery});
-    return areas.map(area => AreaResponse.fromSchema(area))
+    const areas = await promiseHandler(
+        AreaSchema.findAll({where: whereQuery})
+    );
+
+    // Unhandled error occurred
+    if (areas.isErr()) return Err(areas.error);
+
+    return Ok(
+        areas.value.map(area => AreaResponse.fromSchema(area))
+    );
 }
 
-export const findArea = async (specifications: Criteria): Promise<AreaResponse | null> => {
-    const whereQuery = specificationBuilder.buildWhereClauseFromSpecifications(specifications);
-    return await AreaSchema.findOne({where: whereQuery});
-}
+type FindAreaErrors = AreaErrors.AREA_NOT_FOUND | AreaErrors.AREA_CANNOT_BE_FOUND_ERROR;
 
-export const createArea = async (createAreaDTO: CreateAreaDTO, branchId: string): Promise<AreaResponse> => {
-    const newArea = await AreaSchema.create({
-        areaId: uuidv4(),
-        branchId,
-        name: createAreaDTO.name,
-        description: createAreaDTO.description,
-    });
-
-    return AreaResponse.fromSchema(newArea);
-}
-
-export const updateArea = async (updateAreaDTO: UpdateAreaDTO, specifications: Criteria): Promise<number> => {
+export const findArea = async (specifications: Criteria): Promise<Result<AreaSchema, FindAreaErrors>> => {
     const whereQuery = specificationBuilder.buildWhereClauseFromSpecifications(specifications);
 
-    const [ affectedFields ] = await AreaSchema.update(updateAreaDTO, { where: whereQuery });
+    const result = await promiseHandler(
+        AreaSchema.findOne({where: whereQuery})
+    );
 
-    return affectedFields;
+    // Unhandled error occurred
+    if (result.isErr()) return Err(result.error);
+
+    if (!result.value) return Err(AreaErrors.AREA_NOT_FOUND);
+
+    return Ok(result.value);
 }
 
-export const deleteArea = async (specifications: Criteria): Promise<number> => {
+export const createArea = async (createAreaDTO: CreateAreaDTO, branchId: string): Promise<Result<AreaResponse, AreaErrors.AREA_ERROR_CANNOT_CREATE_AREA>> => {
+
+    const result = await promiseHandler(
+        AreaSchema.create({
+            areaId: uuidv4(),
+            branchId,
+            name: createAreaDTO.name,
+            description: createAreaDTO.description,
+        })
+    );
+
+    // If an unhandled error occurred, return an error
+    if (result.isErr()) return Err(result.error);
+
+    return Ok(
+        AreaResponse.fromSchema(result.value)
+    );
+}
+
+export const updateArea = async (updateAreaDTO: UpdateAreaDTO, specifications: Criteria): Promise<Result<number, AreaErrors.AREA_NOT_UPDATED>> => {
     const whereQuery = specificationBuilder.buildWhereClauseFromSpecifications(specifications);
 
-    return await AreaSchema.destroy({where: whereQuery});
+    const result = await promiseHandler(
+        AreaSchema.update(updateAreaDTO, { where: whereQuery })
+    );
+
+    // Unhandled error occurred
+    if (result.isErr()) return Err(result.error);
+
+    // Known error occurred (area not found)
+    if (result.value[0] === 0) return Err(AreaErrors.AREA_NOT_UPDATED);
+
+    return Ok(result.value[0]);
+}
+
+export const deleteArea = async (specifications: Criteria): Promise<Result<number, AreaErrors.AREA_ERROR_CANNOT_DELETE_AREA>> => {
+    const whereQuery = specificationBuilder.buildWhereClauseFromSpecifications(specifications);
+
+    const result = await promiseHandler(
+        AreaSchema.destroy({where: whereQuery})
+    );
+
+    // Unhandled error occurred
+    if (result.isErr()) return Err(result.error);
+
+    return Ok(result.value);
 }
