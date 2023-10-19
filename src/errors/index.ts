abf48738de0f0e6fd58d3613441cd35f13c75177
LimbersMay <limbermay1@gmail.com>
2023-10-19 16:48:59 -0600

    (barrel errors, helpers) archivos de barril actualizados

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,5 +1,5 @@
 export * from './auth.errors';
 export * from './user.errors';
 export * from './table.errors';
-export * from './menu.errors';
+export * from './menu.error';
 export * from './order.errors';
