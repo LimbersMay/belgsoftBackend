175844a81b5424c1d9c4376680ccb9eee061c070
LimbersMay <limbermay1@gmail.com>
2023-10-19 16:48:37 -0600

    (services menu.service) Posibles errores de las operaciones del service manejados con el objeto Result

--- src/services/menu.service.ts
+++ src/services/menu.service.ts
@@ -1,84 +1,118 @@
 import {v4 as uuidv4} from "uuid";
+import {Err, Ok, Result} from "ts-results-es";
 import {CategorySchema, MenuSchema} from "../models";
 import {MenuResponse} from "../mappers";
 import {CreateMenuDTO} from "../controllers/menu/validations/menu.create";
 import {UpdateMenuDTO} from "../controllers/menu/validations/menu.update";
-import {MenuErrors} from "../errors";
+import {MenuError} from "../errors";
 import {Criteria, SequelizeSpecificationBuilder} from "../specifications";
+import {promiseHandler} from "../helpers";
 
 const specificationBuilder = new SequelizeSpecificationBuilder();
 
 
-export const findAllMenu = async (specifications: Criteria): Promise<MenuResponse[]> => {
+export const findAllMenu = async (specifications: Criteria): Promise<Result<MenuResponse[], Error>> => {
 
     const whereClause = specificationBuilder.buildWhereClauseFromSpecifications(specifications);
 
-    const menu = await MenuSchema.findAll({
-        where: whereClause,
-        include: [
-            {model: CategorySchema, as: 'category'}
-        ]
-    });
-    return menu.map(item => MenuResponse.fromMenu(item));
+    const menusResult = await promiseHandler(
+        MenuSchema.findAll({
+            where: whereClause,
+            include: [
+                {model: CategorySchema, as: 'category'}
+            ]
+        })
+    );
+
+    if (menusResult.isErr()) return menusResult.error;
+
+    return Ok(
+        menusResult.value.map(item => MenuResponse.fromMenu(item))
+    );
 }
 
-export const findOneMenu = async (specifications: Criteria) => {
+export const findOneMenu = async (specifications: Criteria): Promise<Result<MenuResponse, MenuError.MENU_NOT_FOUND>> => {
     const whereClause = specificationBuilder.buildWhereClauseFromSpecifications(specifications);
 
-    const menu = await MenuSchema.findOne({
-        where: whereClause,
-        include: [
-            {model: CategorySchema, as: 'category'}
-        ]
-    });
+    const menuResult = await promiseHandler(
+        MenuSchema.findOne({
+            where: whereClause,
+            include: [
+                {model: CategorySchema, as: 'category'}
+            ]
+        })
+    );
 
-    if (!menu){
-        return MenuErrors.MENU_NOT_FOUND
-    }
+    if (menuResult.isErr()) return Err(menuResult.error);
 
-    return MenuResponse.fromMenu(menu);
+    if (!menuResult.value) return Err(MenuError.MENU_NOT_FOUND);
+
+    return Ok(MenuResponse.fromMenu(menuResult.value));
 }
 
-export const createMenu = async (menuDTO: CreateMenuDTO, branchId: string) => {
-    const newMenu = await MenuSchema.create({
-        menuId: uuidv4(),
-        branchId,
-        categoryId: menuDTO.categoryId,
-        name: menuDTO.name,
-        description: menuDTO.description,
-        price: menuDTO.price,
-        image: menuDTO.image,
-        isAvailable: menuDTO.isAvailable,
-    });
+export const createMenu = async (menuDTO: CreateMenuDTO, branchId: string): Promise<Result<MenuResponse, unknown>> => {
 
-    await newMenu.reload({
+    const newMenu = await promiseHandler(
+        MenuSchema.create({
+            menuId: uuidv4(),
+            branchId,
+            categoryId: menuDTO.categoryId,
+            name: menuDTO.name,
+            description: menuDTO.description,
+            price: menuDTO.price,
+            image: menuDTO.image,
+            isAvailable: menuDTO.isAvailable,
+        })
+    );
+
+    if (newMenu.isErr()) return newMenu.error;
+
+    await newMenu.value.reload({
         include: [
             {model: CategorySchema, as: 'category'}
         ]
     })
 
-    return MenuResponse.fromMenu(newMenu);
+    return Ok(MenuResponse.fromMenu(newMenu.value));
 }
 
-export const updateMenu = async (menuDTO: UpdateMenuDTO, specifications: Criteria) => {
+export const updateMenu = async (menuDTO: UpdateMenuDTO, specifications: Criteria): Promise<Result<number, MenuError.MENU_NOT_UPDATED>> => {
 
     const whereClause = specificationBuilder.buildWhereClauseFromSpecifications(specifications);
 
-    const [ affectedFields ] = await MenuSchema.update(
-        {...menuDTO},
-        {
-            where: whereClause
-        }
+    const updateResult = await promiseHandler(
+        MenuSchema.update(
+            {...menuDTO},
+            {
+                where: whereClause
+            }
+        )
     );
 
-    return affectedFields;
+    if (updateResult.isErr()) return updateResult.error;
+
+    if (updateResult.value[0] === 0) return Err(MenuError.MENU_NOT_UPDATED);
+
+    return Ok(
+        updateResult.value[0]
+    );
 }
 
-export const deleteMenu = async (specifications: Criteria) => {
+export const deleteMenu = async (specifications: Criteria): Promise<Result<number, MenuError.MENU_NOT_DELETED>> => {
 
     const whereClause = specificationBuilder.buildWhereClauseFromSpecifications(specifications);
 
-    return await MenuSchema.destroy({
-        where: whereClause
-    });
+    const deleteResult = await promiseHandler(
+        MenuSchema.destroy({
+            where: whereClause
+        })
+    );
+
+    if (deleteResult.isErr()) return deleteResult.error;
+
+    if (deleteResult.value === 0) return Err(MenuError.MENU_NOT_DELETED);
+
+    return Ok(
+        deleteResult.value
+    );
 }
