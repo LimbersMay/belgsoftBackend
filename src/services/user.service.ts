38e939a63443fd4ece88d1a310d2151c25dee6f7
LimbersMay <limbermay1@gmail.com>
2023-11-16 09:50:57 -0600

    (services user.service) Se implementó la operación de eliminar un usuario haciendo un "soft delete"

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -12,32 +12,6 @@ import {promiseHandler} from "../helpers";
 // User specification validator
 const specificationBuilder = new SequelizeSpecificationBuilder();
 
-export const updateUser = async (updateUserDTO: UpdateUserDTO, specifications: Criteria): Promise<Result<number, UserError.USER_NOT_UPDATED>> => {
-
-    // If the password is updated, encrypt it
-    if (updateUserDTO.password) {
-        updateUserDTO.password = await encrypt(updateUserDTO.password);
-    }
-
-    const whereQuery = specificationBuilder.buildWhereClauseFromSpecifications(specifications);
-
-    const result = await promiseHandler(
-        UserSchema.update({
-            ...updateUserDTO
-        }, {
-            where: {
-                ...whereQuery
-            }
-        })
-    );
-
-    if (result.isErr()) return Err(result.error);
-
-    if (result.value[0] === 0) return Err(UserError.USER_NOT_UPDATED);
-
-    return Ok(result.value[0]);
-}
-
 type UserFinderErrors = UserError.USER_NOT_FOUND | UserError.USER_ERROR_CANNOT_GET_USER;
 
 export const findUser = async (specification: Criteria): Promise<Result<UserSchema, UserFinderErrors>> => {
@@ -79,4 +53,49 @@ export const findAllUsers = async (specification: Criteria): Promise<Result<User
     return Ok(
         users.value.map(user => UserResponse.fromUser(user))
     );
+}
+
+export const updateUser = async (updateUserDTO: UpdateUserDTO, specifications: Criteria): Promise<Result<number, UserError.USER_NOT_UPDATED>> => {
+
+    // If the password is updated, encrypt it
+    if (updateUserDTO.password) {
+        updateUserDTO.password = await encrypt(updateUserDTO.password);
+    }
+
+    const whereQuery = specificationBuilder.buildWhereClauseFromSpecifications(specifications);
+
+    const result = await promiseHandler(
+        UserSchema.update({
+            ...updateUserDTO
+        }, {
+            where: {
+                ...whereQuery
+            }
+        })
+    );
+
+    if (result.isErr()) return Err(result.error);
+
+    if (result.value[0] === 0) return Err(UserError.USER_NOT_UPDATED);
+
+    return Ok(result.value[0]);
+}
+
+export const deleteUser = async (specifications: Criteria): Promise<Result<number, UserError.USER_ERROR_CANNOT_DELETE_USER>> => {
+
+    const whereQuery = specificationBuilder.buildWhereClauseFromSpecifications(specifications);
+
+    const result = await promiseHandler(
+        UserSchema.update({
+            userStateId: '4'
+        }, {
+            where: whereQuery
+        })
+    );
+
+    if (result.isErr()) return Err(result.error);
+
+    if (result.value[0] === 0) return Err(UserError.USER_ERROR_CANNOT_DELETE_USER);
+
+    return Ok(result.value[0]);
 }
